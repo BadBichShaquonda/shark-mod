eceae4445e88f676258f2681691274f4ea72e5ca
Michael Masciotra <michael.masciotra.18@gmail.com>
2020-05-31 22:25:47 -0400

    Created raw and cooked shark meat, no textures yet.

--- src/main/java/com/eon/sharkmod/SharkMod.java
+++ src/main/java/com/eon/sharkmod/SharkMod.java
@@ -1,22 +1,20 @@
 package com.eon.sharkmod;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+import com.eon.sharkmod.init.ItemInit;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("eonshark")
@@ -27,10 +25,9 @@ public class SharkMod {
 	public static SharkMod instance;
 
 	public SharkMod() {
-		// Register the setup method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-		// Register the doClientStuff method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::setup);
+		modEventBus.addListener(this::doClientStuff);
 		
 		// Register ourselves for server and other game events we are interested in
 		MinecraftForge.EVENT_BUS.register(this);
@@ -54,5 +51,18 @@ public class SharkMod {
 	public void onServerStarting(FMLServerStartingEvent event) {
 		// do something when the server starts
 		LOGGER.info("HELLO from server starting");
+	}
+	
+	public static class SharkItemGroup extends ItemGroup
+	{
+		public static final SharkItemGroup instance = new SharkItemGroup(ItemGroup.GROUPS.length, "eonsharktab");
+		private SharkItemGroup(int index, String label) {
+			super(index, label);
+		}
+		
+		@Override
+		public ItemStack createIcon() {
+			return new ItemStack(ItemInit.shark_fin);
+		}
 	}
 }
--- src/main/java/com/eon/sharkmod/init/ItemInit.java
+++ src/main/java/com/eon/sharkmod/init/ItemInit.java
@@ -1,9 +1,12 @@
 package com.eon.sharkmod.init;
 
 import com.eon.sharkmod.SharkMod;
+import com.eon.sharkmod.SharkMod.SharkItemGroup;
 
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,9 +17,20 @@ import net.minecraftforge.registries.ObjectHolder;
 @ObjectHolder(SharkMod.MOD_ID)
 public class ItemInit {
 	public static final Item shark_fin = null;
+	public static final Item raw_shark_meat = null;
+	public static final Item cooked_shark_meat = null;
 	
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
-		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("shark_fin"));
+		event.getRegistry().register(new Item(new Item.Properties().group(SharkItemGroup.instance))
+				.setRegistryName("shark_fin"));
+		
+		event.getRegistry().register(new Item(new Item.Properties().group(SharkItemGroup.instance)
+				.food(new Food.Builder().hunger(2).saturation(1F).effect(() -> new EffectInstance(Effects.HUNGER, 200, 0), 0.85F).build()))
+				.setRegistryName("raw_shark_meat"));
+		
+		event.getRegistry().register(new Item(new Item.Properties().group(SharkItemGroup.instance)
+				.food(new Food.Builder().hunger(8).saturation(4F).build()))
+				.setRegistryName("cooked_shark_meat"));
 	}
 }
